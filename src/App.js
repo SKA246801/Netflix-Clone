7d2ac64d4541f059f3b26a38c43e0319b0650ea5
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-10 18:59:10 -0500

    created login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css'
 // import Home from './Pages/Home/Home'
 // import Watch from './Pages/Watch/Watch'
-import Register from './Pages/Register/Register'
+// import Register from './Pages/Register/Register'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
     <div className='App'>
       {/* <Home />
       <Watch /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   )
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,7 +2,27 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
-  return <div>Login</div>
+  return (
+    <div className='login'>
+      <div className='top'>
+        <img src={require('../../Assets/Images/netflix-logo.png')} className='login-logo' alt='' />
+      </div>
+      <div className='login-container'>
+        <form className='login-form'>
+          <h1>Sign In</h1>
+          <input type='email' placeholder='Email or phone number' />
+          <input type='password' placeholder='Password' />
+          <button className='login-btn'>Sign In</button>
+          <span>
+            New to Netflix? <b>Sign up now</b>.
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
