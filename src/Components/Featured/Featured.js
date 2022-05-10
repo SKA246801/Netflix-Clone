ce067ef2c74acaaee824a3bf4d80f3d9114f7c01
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-10 09:26:54 -0500

    featured section completed

---
+++ src/Components/Featured/Featured.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Featured.css'
+
+import { InfoOutlined, PlayArrow } from '@mui/icons-material'
+
+function Featured({ type }) {
+  return (
+    <div className='featured'>
+      {type && (
+        <div className='category'>
+          <span>{type === 'movies' ? 'Movies' : 'Series'}</span>
+          <select name='genre' id='genre'>
+            <option value='genre'>Genre</option>
+            <option value='adventure'>Adventure</option>
+            <option value='comedy'>Comedy</option>
+            <option value='crime'>Crime</option>
+            <option value='fantasy'>Fantasy</option>
+            <option value='historical'>Historical</option>
+            <option value='horror'>Horror</option>
+            <option value='romance'>Romance</option>
+            <option value='sci-fi'>Sci-fi</option>
+            <option value='thriller'>Thriller</option>
+            <option value='western'>Western</option>
+            <option value='animation'>Animation</option>
+            <option value='drama'>Drama</option>
+            <option value='documentary'>Documentary</option>
+          </select>
+        </div>
+      )}
+      <img src={require('../../Assets/Images/marvel.jpg')} alt='' />
+
+      <div className='info'>
+        <img
+          src={require('../../Assets/Images/marvel.jpg')}
+          className='movie-img'
+          alt='movie'
+        />
+        <span className='description'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores,
+          autem ex accusantium magni tenetur incidunt quasi nostrum repellendus
+          voluptatem omnis velit voluptatibus est aspernatur laboriosam odio
+          fuga aliquam vel blanditiis.
+        </span>
+        <div className='buttons'>
+          <button className='play'>
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className='more'>
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,89 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../Navbar/Navbar'
+import Featured from '../Featured/Featured'
 
 function Home() {
   return (
     <div className='home'>
       <Navbar />
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nihil
-      repudiandae libero optio minus perferendis cumque fuga enim tempore iure
-      doloremque eveniet quaerat asperiores dignissimos, quae mollitia labore?
-      Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing
-      elit. At nihil repudiandae libero optio minus perferendis cumque fuga enim
-      tempore iure doloremque eveniet quaerat asperiores dignissimos, quae
-      mollitia labore? Neque, dignissimos? Lorem ipsum, dolor sit amet
-      consectetur adipisicing elit. At nihil repudiandae libero optio minus
-      perferendis cumque fuga enim tempore iure doloremque eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? Lorem
-      ipsum, dolor sit amet consectetur adipisicing elit. At nihil repudiandae
-      libero optio minus perferendis cumque fuga enim tempore iure doloremque
-      eveniet quaerat asperiores dignissimos, quae mollitia labore? Neque,
-      dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing elit. At
-      nihil repudiandae libero optio minus perferendis cumque fuga enim tempore
-      iure doloremque eveniet quaerat asperiores dignissimos, quae mollitia
-      labore? Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit. At nihil repudiandae libero optio minus perferendis
-      cumque fuga enim tempore iure doloremque eveniet quaerat asperiores
-      dignissimos, quae mollitia labore? Neque, dignissimos? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. At nihil repudiandae libero optio
-      minus perferendis cumque fuga enim tempore iure doloremque eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? Lorem
-      ipsum, dolor sit amet consectetur adipisicing elit. At nihil repudiandae
-      libero optio minus perferendis cumque fuga enim tempore iure doloremque
-      eveniet quaerat asperiores dignissimos, quae mollitia labore? Neque,
-      dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing elit. At
-      nihil repudiandae libero optio minus perferendis cumque fuga enim tempore
-      iure doloremque eveniet quaerat asperiores dignissimos, quae mollitia
-      labore? Neque, dignissimos? eveniet quaerat asperiores dignissimos, quae
-      mollitia labore? Neque, dignissimos? Lorem ipsum, dolor sit amet
-      consectetur adipisicing elit. At nihil repudiandae libero optio minus
-      perferendis cumque fuga enim tempore iure doloremque eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? eveniet
-      quaerat asperiores dignissimos, quae mollitia labore? Neque, dignissimos?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nihil
-      repudiandae libero optio minus perferendis cumque fuga enim tempore iure
-      doloremque eveniet quaerat asperiores dignissimos, quae mollitia labore?
-      Neque, dignissimos? eveniet quaerat asperiores dignissimos, quae mollitia
-      labore? Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit. At nihil repudiandae libero optio minus perferendis
-      cumque fuga enim tempore iure doloremque eveniet quaerat asperiores
-      dignissimos, quae mollitia labore? Neque, dignissimos? eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? Lorem
-      ipsum, dolor sit amet consectetur adipisicing elit. At nihil repudiandae
-      libero optio minus perferendis cumque fuga enim tempore iure doloremque
-      eveniet quaerat asperiores dignissimos, quae mollitia labore? Neque,
-      dignissimos? eveniet quaerat asperiores dignissimos, quae mollitia labore?
-      Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing
-      elit. At nihil repudiandae libero optio minus perferendis cumque fuga enim
-      tempore iure doloremque eveniet quaerat asperiores dignissimos, quae
-      mollitia labore? Neque, dignissimos? eveniet quaerat asperiores
-      dignissimos, quae mollitia labore? Neque, dignissimos? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. At nihil repudiandae libero optio
-      minus perferendis cumque fuga enim tempore iure doloremque eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? eveniet
-      quaerat asperiores dignissimos, quae mollitia labore? Neque, dignissimos?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nihil
-      repudiandae libero optio minus perferendis cumque fuga enim tempore iure
-      doloremque eveniet quaerat asperiores dignissimos, quae mollitia labore?
-      Neque, dignissimos? eveniet quaerat asperiores dignissimos, quae mollitia
-      labore? Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit. At nihil repudiandae libero optio minus perferendis
-      cumque fuga enim tempore iure doloremque eveniet quaerat asperiores
-      dignissimos, quae mollitia labore? Neque, dignissimos? eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos? Lorem
-      ipsum, dolor sit amet consectetur adipisicing elit. At nihil repudiandae
-      libero optio minus perferendis cumque fuga enim tempore iure doloremque
-      eveniet quaerat asperiores dignissimos, quae mollitia labore? Neque,
-      dignissimos? eveniet quaerat asperiores dignissimos, quae mollitia labore?
-      Neque, dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing
-      elit. At nihil repudiandae libero optio minus perferendis cumque fuga enim
-      tempore iure doloremque eveniet quaerat asperiores dignissimos, quae
-      mollitia labore? Neque, dignissimos? eveniet quaerat asperiores
-      dignissimos, quae mollitia labore? Neque, dignissimos? Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. At nihil repudiandae libero optio
-      minus perferendis cumque fuga enim tempore iure doloremque eveniet quaerat
-      asperiores dignissimos, quae mollitia labore? Neque, dignissimos?
+      <Featured />
     </div>
   )
 }
