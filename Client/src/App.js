bbab292f1c7a87663b6169cd798ee4c15abd8732
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-12 16:00:32 -0500

    added redirect to protect routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home/Home'
 import Watch from './Pages/Watch/Watch'
@@ -7,16 +7,17 @@ import Register from './Pages/Register/Register'
 import Login from './Pages/Login/Login'
 
 function App() {
+  const user = true
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/movies' element={<Home type='movies' />} />
-          <Route path='/series' element={<Home type='series' />} />
-          <Route path='/watch' element={<Watch />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
+          <Route exact path='/' element={user ? <Home /> : <Navigate to='/register' />} />
+          <Route path='/movies' element={user ? <Home type='movies' /> : <Navigate to='/register' />} />
+          <Route path='/series' element={user ? <Home type='series' /> : <Navigate to='/register' />} />
+          <Route path='/watch' element={user ? <Watch /> : <Navigate to='/register' />} />
+          <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
         </Routes>
       </div>
     </Router>
