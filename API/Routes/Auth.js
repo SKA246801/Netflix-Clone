e421126da202309b1c7836cb8bf9783b3b8ee778
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-12 10:25:14 -0500

    created login route

--- API/Routes/Auth.js
+++ API/Routes/Auth.js
@@ -18,4 +18,23 @@ router.post('/register', async (req, res) => {
   }
 })
 
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return res.status(401).json('Wrong username or password!fdsfdsaa')
+    }
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.sometext2)
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8)
+    if (originalPassword !== req.body.password) {
+      return res.status(401).json('Wrong username or password!')
+    }
+    const { password, ...info } = user._doc
+    return res.status(200).json(info)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+})
+
 module.exports = router
