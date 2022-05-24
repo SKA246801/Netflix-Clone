50cfe96830fa383481890ad70dfc0eb00a0510e9
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-24 16:24:20 -0500

    fixed up styling for featured movie and linked play button to trailer

--- Client/src/Components/Featured/Featured.js
+++ Client/src/Components/Featured/Featured.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Featured.css'
-
 import { InfoOutlined, PlayArrow } from '@mui/icons-material'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function Featured({ type, setGenre }) {
   const [content, setContent] = useState({})
@@ -36,29 +36,23 @@ function Featured({ type, setGenre }) {
             <option value='comedy'>Comedy</option>
             <option value='crime'>Crime</option>
             <option value='family'>Family</option>
-            <option value='historical'>Historical</option>
             <option value='horror'>Horror</option>
-            <option value='romance'>Romance</option>
             <option value='sci-fi'>Sci-fi</option>
-            <option value='thriller'>Thriller</option>
-            <option value='western'>Western</option>
-            <option value='animation'>Animation</option>
             <option value='drama'>Drama</option>
-            <option value='documentary'>Documentary</option>
           </select>
         </div>
       )}
       <img src={content.img} className='background-img' alt='' />
-
       <div className='info'>
-        {/* <img src={content.img} className='featured-img' alt='movie' /> */}
         <h1 className='featured-img-title'>{content.title}</h1>
         <span className='featured-description'>{content.description}</span>
         <div className='buttons'>
-          <button className='play'>
-            <PlayArrow />
-            <span>Play</span>
-          </button>
+          <Link to='/watch' state={content} style={{ textDecoration: 'none' }}>
+            <button className='play'>
+              <PlayArrow />
+              <span>Play</span>
+            </button>
+          </Link>
           <button className='more'>
             <InfoOutlined />
             <span>Info</span>
